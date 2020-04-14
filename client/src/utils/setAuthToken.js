85a2eb1fd2017076d1db90858bdc06613e959cf1
audrey tang <audreytang28@gmail.com>
2020-04-14 14:34:14 +0800

    First commit

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+// taking token passed in to headers
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common['x-auth-token'] = token;
+  } else {
+    delete axios.defaults.headers.common['x-auth-token'];
+  }
+};
+
+export default setAuthToken;
